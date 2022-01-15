aaf4bba97d759871fb83077d4e62d96fc2a8700d
Argo-Tianyi <argo@163.com>
2022-01-15 10:23:13 +0800

    revert: 回滚 ColocPicker 组件更新

--- src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
+++ src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-using Microsoft.AspNetCore.Components;
-
 namespace BootstrapBlazor.Components;
 
 /// <summary>
@@ -17,12 +15,4 @@ public partial class ColorPicker
     protected string? ClassName => CssBuilder.Default("form-control")
         .AddClass(CssClass).AddClass(ValidCss)
         .Build();
-
-
-    /// <summary>
-    /// 获得/设置 是否显示颜色文字标签 默认为 true
-    /// </summary>
-    [Parameter]
-    public bool IsShowColorTextLabel { get; set; }=true;
-
 }
